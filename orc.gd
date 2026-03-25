6331f726a540bf62919c7adba5e1b75d0a21303a
mittens <amluck88@gmail.com>
2026-03-25 12:04:54 -0400

    added Orc with chase logic

---
+++ orc.gd
@@ -0,0 +1,193 @@
+extends CharacterBody2D
+
+enum EnemyState { IDLE_LEFT, IDLE_RIGHT, WALK, ATTACK, DIE }
+var state: EnemyState = EnemyState.WALK
+
+@export var enemy_data: EnemyData
+
+@onready var ground_check := $RayCast2D
+@onready var attack := $Attack/Hitbox
+@onready var axe_attack := $AxeAttack
+@onready var axe_attack_hitbox := $AxeAttack/Hitbox
+@onready var health_component := $HealthComponent
+@onready var hurtbox := $Hurtbox
+@onready var sprite := $AnimatedSprite2D
+
+signal enemy_died()
+
+var room_controller: Node2D
+
+var gravity: float
+var speed: float
+var blood_gradient: Gradient
+var blood_scene: PackedScene
+
+var facing_direction := 1
+var turn_cooldown := 0.1
+var turn_timer := 0.0
+
+var player: Node2D = null
+var player_in_range := false
+var can_attack := true
+var is_attacking := false
+
+var last_hit_source_pos: Vector2 = Vector2.ZERO
+
+func _ready():
+	apply_enemy_data()
+	
+	hurtbox.hit_received.connect(_on_hit_received)
+	health_component.died.connect(_on_died)
+	
+	axe_attack_hitbox.monitorable = false
+	axe_attack_hitbox.monitoring = false
+
+func _physics_process(delta):
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if is_attacking:
+		velocity.x = 0
+	else:
+		if player_in_range:
+			chase_player()
+		else:
+			patrol(delta)
+		
+	ground_check.scale.x = 1 if (facing_direction == 1) else -1
+	axe_attack.scale.x = 1 if (facing_direction == 1) else -1
+
+	move_and_slide()
+
+	match state:
+		EnemyState.IDLE_LEFT:
+			sprite.play("idle_left")
+		EnemyState.IDLE_RIGHT:
+			sprite.play("idle_right")
+		EnemyState.WALK:
+			if not is_attacking:
+				if player_in_range:
+					sprite.set_speed_scale(2)
+					velocity.x = facing_direction * speed * 1.5
+				else:
+					sprite.set_speed_scale(1)
+					velocity.x = facing_direction * speed
+				if facing_direction == -1:
+					sprite.play("walk_left")
+				elif facing_direction == 1:
+					sprite.play("walk_right")
+		EnemyState.ATTACK:
+			velocity.x = 0
+			sprite.set_speed_scale(1)
+			if facing_direction == -1:
+				sprite.play("attack_left")
+			elif facing_direction == 1:
+				sprite.play("attack_right")
+		EnemyState.DIE:
+			velocity.x = 0
+			remove_from_group("enemies")
+	if is_attacking:
+		state = EnemyState.ATTACK
+	elif not is_attacking:
+		axe_attack_hitbox.monitorable = false
+		axe_attack_hitbox.monitoring = false
+			
+func apply_enemy_data():
+	health_component.initialize(enemy_data.max_health, enemy_data.max_health)
+	speed = enemy_data.speed
+	gravity = enemy_data.gravity
+
+func _on_hit_received(attack_data, source_position: Vector2):
+	last_hit_source_pos = source_position
+	health_component.damage(attack_data.damage)
+	apply_knockback(attack_data.knockback, source_position)
+	
+func apply_knockback(force, source_position: Vector2):
+	var knockback_dir = (global_position - source_position).normalized()
+	velocity = knockback_dir * force
+
+func _on_died():
+	var hit_position = global_position
+	call_deferred("spawn_blood", hit_position, last_hit_source_pos)
+
+	if room_controller:
+		room_controller.on_enemy_died(self)
+	state = EnemyState.DIE
+	Events.entity_died.emit(self)
+	enemy_died.emit()
+	print("Orc killed.")
+	queue_free()
+
+func turn():
+	facing_direction *= -1
+	turn_timer = turn_cooldown
+	
+func spawn_blood(hit_position: Vector2, source_pos: Vector2):
+	var blood = enemy_data.blood_scene.instantiate()
+	if source_pos == Vector2.ZERO:
+		source_pos = hit_position
+	
+	var direction = hit_position - source_pos
+	
+	if direction.length() == 0:
+		direction = Vector2.RIGHT
+	else:
+		direction = direction.normalized()
+
+	get_tree().current_scene.add_child(blood)
+	blood.set_gradient(enemy_data.blood_gradient)
+	blood.global_position = hit_position
+	blood.set_direction(direction)
+	
+	blood.restart()
+
+func _on_detection_body_entered(body):
+	if body.is_in_group("player"):
+		player = body
+		player_in_range = true
+
+func _on_detection_body_exited(body):
+	if body.is_in_group("player"):
+		player = null
+		player_in_range = false
+
+func chase_player():
+	if player == null:
+		return
+	
+	var distance = global_position.distance_to(player.global_position)
+	
+	if distance < 40:
+		if can_attack and not is_attacking:
+			try_attack()
+			return
+		
+	facing_direction = sign(player.global_position.x - global_position.x)
+	state = EnemyState.WALK
+
+func try_attack():
+	if can_attack:
+		can_attack = false
+		is_attacking = true
+		axe_attack_hitbox.monitoring = true
+		axe_attack_hitbox.monitorable = true
+		state = EnemyState.ATTACK
+
+func _on_animated_sprite_2d_animation_finished():
+	if sprite.animation == "attack_left" or sprite.animation == "attack_right":
+		axe_attack_hitbox.monitoring = false
+		axe_attack_hitbox.monitorable = false
+		is_attacking = false
+		
+		await get_tree().create_timer(1.5).timeout
+		can_attack = true
+		
+func patrol(delta):
+	turn_timer -= delta
+	if turn_timer <= 0:
+		if is_on_wall():
+			turn()
+		elif is_on_floor() and not ground_check.is_colliding():
+			turn()
+	state = EnemyState.WALK
--- resources/Configs/AttackData.gd
+++ resources/Configs/AttackData.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name AttackData
 
-@export var damage: int = 1
+@export var damage: int
 @export var knockback: float = 200
 @export var hitstun: float = 0.15
 @export var hit_sound: String = ""
